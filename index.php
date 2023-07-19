18748cca734a018954f7343ef8d0d7145c700e53
henzoparahua <hnz.2894henzop19@gmail.com>
2023-07-19 02:24:04 -0300

    oooooooooomagaaaaaah -- att

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HelpFox</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="icon" type="imagex/png" href="styles/Logo.png">
+ </head>
+<body>
+    <nav class="navbarmenu">
+    <section class="LogoContainer"><a href="#"><img src="styles/header_helpfox.png" alt="HelpfoxLogo"></a></section>
+    <section class="NavbarLinkContainer"><ul><a href="#"><li>Inicio</li></a><a href="#"><li>Produtos</li></a><a href="#"><li>Sobre</li></a><a href="#"><li>Contato</li></a></ul></section>
+    </nav>
+    <section id="WrapperBG2">
+        <section id="WrapperBG">
+        <article class="HeaderBox_1">
+                <h3>Aonde transformamos necessidade em inovação</h3>
+            </article>
+        </section>
+    </section>
+
+    <section class="WrapperInside">
+        <div class="LogoImg"></div>
+        <section class="ArticleWrapper">
+
+            <article class="ArticleBox_2">
+                <h3>Como funciona?</h3>
+            </article>
+        </section>
+        <footer class="footer1"><p>©2023 HelpFox Ltd. Criado por Henzo Possebon Ayres.</p></footer>
+    </section>
+</body>
+</html>
